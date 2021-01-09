fe911428402e74bf4b23aba4d8821c03d279fff8
Leandro Klaus <Leandro Klaus@Leandros-Pc.Klaus.local>
2021-01-09 17:30:18 +0100

    Create setup_autoupdate.sh

---
+++ setup_autoupdate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+echo "Enter Database name to be created:"
+read database
+
+
+# Clear Current Screen
+clear
+
+# Check Session Status
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+elif [[ $EUID -eq 0 ]]; then
+   echo -e "Session Running as \e[36mROOT\e[0m"
+fi
+
+# Update Local System Packages 
+apt update && apt -y upgrade
+
+#install Auto Update
+apt -y install unattended-upgrades apt-listchanges bsd-mailx
+
+#Enable Auto Update
+dpkg-reconfigure -plow unattended-upgrades
