4bd2da80be739999e89b3d33da8dd6cabeef9c8d
Leandro Klaus <Leandro Klaus@Leandros-Pc.Klaus.local>
2021-01-15 00:09:40 +0100

    Update

---
+++ special/scheduled_scripts/pihole_teleporter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+# Clear Current Screen
+clear
+
+# Check Session Status
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+elif [[ $EUID -eq 0 ]]; then
+   echo -e "Session Running as \e[36mROOT\e[0m"
+fi
+
+
+#Create Teleporter Backup
+set -e
+backupfolder="/mnt/nas"
+DATE=$(date "+%Y-%m-%d_%H-%M-%S")
+php /var/www/html/admin/scripts/pi-hole/php/teleporter.php > "$backupfolder/pi-hole-teleporter_$DATE.tar.gz"
+find "$backupfolder" -mtime +7 -delete
+echo $DATE
+
