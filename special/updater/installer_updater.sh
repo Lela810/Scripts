a66bb421e92fd8cec4a023b1a5dcc08ce944d839
Leandro Klaus <Leandro Klaus@Leandros-Pc.Klaus.local>
2021-02-22 23:50:20 +0100

    Update

--- special/updater/installer_updater.sh
+++ special/updater/installer_updater.sh
@@ -4,13 +4,6 @@
 # Clear Current Screen
 clear
 
-# Check Session Status
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-elif [[ $EUID -eq 0 ]]; then
-   echo -e "Session Running as \e[36mROOT\e[0m"
-fi
 
 
 
