0aa71bd3cbcb292b5fa65b6f0950e592ee8c4610
Leandro Klaus <lklaus@online.gibz.ch>
2021-04-28 12:28:44 +0200

    Create Create_Emergency_User.ps1

---
+++ windows/Create_Emergency_User.ps1
@@ -0,0 +1,12 @@
+﻿$Password = Read-Host -Prompt "Emergency Passwort"
+
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+
+New-LocalUser "Emergency" -Password $SecurePassword -FullName "Emergency" -Description "Notfall Account RedIT" -PasswordNeverExpires -AccountNeverExpires
+Add-LocalGroupMember -Group "Administrators" -Member "Emergency"
+
+$UserAccount = Get-LocalUser -Name "Emergency"
+$UserAccount | Set-LocalUser -Password $SecurePassword
+
+pause
