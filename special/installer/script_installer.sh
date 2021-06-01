867e62c5c6721fea1c1c7cf7f1fb3bcf9a81ac5e
Leandro Klaus <lklaus@online.gibz.ch>
2021-06-02 01:30:00 +0200

    Update script_installer.sh

--- special/installer/script_installer.sh
+++ special/installer/script_installer.sh
@@ -32,7 +32,6 @@ cd /tmp/Scripts
 
 
 
-
 clear
 
 
@@ -58,7 +57,8 @@ echo
 echo "Your IP is now set Static!"
 fi
 
-if dpkg -s linux-virtual linux-cloud-tools-virtual linux-tools-virtual; then
+
+if dpkg -s linux-virtual linux-cloud-tools-virtual linux-tools-virtual &> /dev/null; then
 echo
 echo "Do you wanna install Linux Integration Services for Hyper-V? y/n:"
 read ubuntulis
