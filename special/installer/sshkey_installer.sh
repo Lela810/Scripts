db936534db9b0f9fd0d353ebb42c3cb9f4a565e8
Leandro Klaus <lklaus@online.gibz.ch>
2021-06-09 01:37:26 +0200

    Update sshkey_installer.sh

--- special/installer/sshkey_installer.sh
+++ special/installer/sshkey_installer.sh
@@ -12,12 +12,20 @@ elif [[ $EUID -eq 0 ]]; then
    echo -e "Session Running as \e[36mROOT\e[0m"
 fi
 
-
+su administrator
 ssh-keygen -b 4096 -C $HOSTNAME
+exit
 
-
-cp /root/.ssh/id_rsa* /home/administrator
+cat /home/administrator/.ssh/id_rsa.pub >> /home/administrator/.ssh/authorized_keys
+cp /home/administrator/.ssh/id_rsa* /home/administrator
+chmod 777 /home/administrator/id_rsa*
 sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
+echo "IgnoreRhosts yes" >> /etc/ssh/sshd_config
+echo "IgnoreUserKnownHosts no" >> /etc/ssh/sshd_config
+echo "StrictModes yes" >> /etc/ssh/sshd_config
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
 
 
 echo ""
