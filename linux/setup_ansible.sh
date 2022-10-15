9bbde2068f3007249fe7e33393dadc7b7f35cb81
Lela810 <lklaus@online.gibz.ch>
2022-10-15 12:46:05 +0200

    Create setup_ansible.sh

---
+++ linux/setup_ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+
+# Clear Current Screen
+clear
+
+# Check Session Status
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+elif [[ $EUID -eq 0 ]]; then
+   echo -e "Session Running as \e[36mROOT\e[0m"
+fi
+
+
+# install updater
+#wget https://raw.githubusercontent.com/lela810/Scripts/master/special/linux/setup_ansible.sh && chmod +x setup_ansible.sh && .\setup_ansible.sh
+
+
+adduser ansible
+passwd ansible
+usermod -aG sudo ansible
